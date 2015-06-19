82a30736884e1784825f0ccd93bc65ddc9f2dd64
xaos <xaos@z-inc.net>
2015-06-19 16:23:40 +0500

    tutorial05

--- src/main/java/com/github/drxaos/robocoder/levels/tutorial/Example05.java
+++ src/main/java/com/github/drxaos/robocoder/levels/tutorial/Example05.java
@@ -7,7 +7,7 @@ import com.github.drxaos.robocoder.program.api.*;
 public class Example05 extends AbstractProgram {
 
     public static void main(String[] args) {
-        Tutorial05Bus.setSkipFramesMax(0);
+        Tutorial05Bus.setSkipFramesMax(10);
         Tutorial05Bus.run(Example05.class);
     }
 
@@ -18,15 +18,22 @@ public class Example05 extends AbstractProgram {
         ArmDriver armDriver = new ArmDriver(bus);
         TurretDriver turretDriver = new TurretDriver(bus);
 
+        double x = 0;
+        double y = 0;
         while (true) {
             basicMovement.sleep(30);
             String recv = bus.request("radio::receive");
-            if (recv != null && !recv.isEmpty()) {
-                System.out.println(recv);
-            } else {
-                System.out.println("------");
+            if (recv == null || recv.isEmpty()) {
+                break;
             }
+            String[] split = recv.split("/");
+            x = Double.parseDouble(split[0]);
+            y = Double.parseDouble(split[1]);
+            basicMovement.move(x - 7, y);
+            basicMovement.move(x, y + 7);
+            basicMovement.move(x + 7, y);
         }
 
+        basicMovement.move(x, y);
     }
 }
--- src/main/java/com/github/drxaos/robocoder/levels/tutorial/Tutorial05Bus.java
+++ src/main/java/com/github/drxaos/robocoder/levels/tutorial/Tutorial05Bus.java
@@ -24,19 +24,33 @@ public class Tutorial05Bus extends AbstractLevel {
 
     @Override
     public void initLevel(Game game) {
-        startPad = new StartPad(new KPoint(-10, 10), 0d);
+        startPad = new StartPad(new KPoint(0, 0), 0d);
         game.addActor(startPad);
 
-        finishPad = new FinishPad(new KPoint(15, 10), 0d);
+        float lx = -7, ly = 0;
+        float x = -7, y = 0;
+
+        for (int i = 0; i < 10; i++) {
+            lx = x;
+            ly = y;
+
+            float a = (float) ((Math.random() - 0.5) * Math.PI * 2 / 3);
+            float d = (float) ((Math.random() * 15) + 25);
+            x = (float) (x + Math.cos(a) * d);
+            y = (float) (y + Math.sin(a) * d);
+
+            InfoStation infoStation = new InfoStation("IS-" + i, new KPoint(lx, ly));
+            infoStation.addDefaultEquipment();
+            infoStation.getEquipment(MemoryEquipment.class).setData("" + x + "/" + y);
+            infoStation.setProgram(Transmitter05.class);
+            game.addActor(infoStation);
+        }
+
+        finishPad = new FinishPad(new KPoint(x, y), 0d);
         game.addActor(finishPad);
 
-        InfoStation infoStation = new InfoStation("IS-01", new KPoint(-6, 10));
-        infoStation.addDefaultEquipment();
-        infoStation.getEquipment(MemoryEquipment.class).setData("Test 1-2-3...");
-        infoStation.setProgram(Transmitter05.class);
-        game.addActor(infoStation);
 
-        robot = new Robot("RC-1", -10, 10, 0);
+        robot = new Robot("RC-1", 0, 0, 0);
         robot.addDefaultEquipment();
         robot.setProgram(userProgram);
         robot.enableLogging();
